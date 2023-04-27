7a5e934c6bc000a957d99964369bb845b1aaa13c
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-04-27 14:42:39 +0200

    Add: axios, Home view & Authentication-Dashborad

---
+++ src/components/Autentication.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        authentication
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Authentication',
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,47 @@
+<template>
+    <Autentication v-if="currentPage === 0"/> <!-- TODO: it has to be changed with v-if="!user" -->
+    <Dashborad v-else/>
+    <button class="btn btn-primary" type="button" @click="changePage">change</button>
+</template>
+
+<script>
+import axios from 'axios';
+import Autentication from '../components/Autentication.vue';
+import Dashborad from '../components/Dashborad.vue';
+
+    export default {
+        name: 'Home',
+
+        components: {Autentication, Dashborad},
+
+        data(){
+            return {
+                currentPage: 0,
+                isUser: false,
+            }
+        },
+
+        methods: {
+            changePage(){
+                this.currentPage === 0 ? this.currentPage = 1 : this.currentPage = 0;
+            }
+        },
+
+        // This is going to be the authentication call
+        // async created(){
+        //     try{
+        //         const result = await axios.post()
+        //         this.isUser = true
+        //     }catch {
+        //         this.isUser = false;
+                
+        //     }
+        // }
+
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
