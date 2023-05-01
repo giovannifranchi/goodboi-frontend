c4b5d9b9250ba2e305be5d8ea6055d6f5c468c1c
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-05-01 02:12:54 +0200

    Add: working api & autotyping component

---
+++ Axios.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+
+const endpoint = 'http://65.108.85.188:3000/api/';
+
--- src/components/Autentication.vue
+++ src/components/Autentication.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="d-flex align-items-center justify-content-center wrapper flex-column">
+    <!-- <AutoTypingComponent /> -->
     <h1 class="mb-5">GoodBoi</h1>
-    <form>
+    <form @submit.prevent="login">
       <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+        <input
+          type="text"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          v-model="username"
+        />
       </div>
       <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" />
+        <input type="password" class="form-control" id="exampleInputPassword1" v-model="password" />
       </div>
       <button type="submit" class="btn btn-primary">login</button>
     </form>
@@ -16,8 +23,45 @@
 </template>
 
 <script>
+import AutoTypingComponent from "./AutoTypingComponent.vue";
+import axios from "axios";
+
 export default {
   name: "Authentication",
+
+  components: { AutoTypingComponent },
+
+  data() {
+    return {
+      username: "",
+      password: "",
+      authToken: null
+    };
+  },
+
+  methods: {
+    login() {
+      const endpoint = "http://65.108.85.188:3000/api/login";
+
+      const payload = {
+        username: this.username,
+        password: this.password,
+      };
+
+      const headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+      };
+
+      axios.post(endpoint,payload, {headers}).then((result) => {
+        this.authToken = result.data.token;
+        this.username = '';
+        this.password = '';
+      });
+
+
+
+    },
+  },
 };
 </script>
 
