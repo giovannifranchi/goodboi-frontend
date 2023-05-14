2f4390272fc375553797de55930c6a1b49bcfe6e
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-05-14 21:02:52 +0200

    Add: spinner for detectors

--- src/components/Dashboard/MainDashboard.vue
+++ src/components/Dashboard/MainDashboard.vue
@@ -3,6 +3,7 @@
     <div class="container">
       <StatsComponentVue class="py-5" />
       <!-- Rev State radios -->
+
       <h3>Revision State</h3>
       <div class="row row-cols-5 w-50">
         <div class="col d-flex justify-content-center" v-for="(radio, index) in revStateFields" :key="index">
@@ -23,32 +24,46 @@
       </div>
       <!-- TODOs: add a spinner component also here to handle the lag in fetchDetectors call ath radio button change -->
       <h3 class="pt-5">Detectors</h3>
-      <div class="d-flex flex-wrap pb-5 pt-3 gy-4 gap-3">
+
+      <div v-if="isDetectorBusy">
+        <AtomSpinner :size="200" color="#fff" />
+      </div>
+      <div class="d-flex flex-wrap pb-5 pt-3 gy-4 gap-3" v-else>
         <div v-for="(detector, index) in orderDetectors" :key="index" class="flex-grow-1">
           <DetectorComponent
             :detectorName="detector.name"
             :detectorCount="detector.count"
             @handleClick="handleDetector"
-            @click="()=>{fetchTables({ detector: this.currentDetector, revState: this.revState, offset: 0 }); fetchAnalysisCount(this.currentDetector)}"
+            @click="
+              () => {
+                fetchTables({ detector: this.currentDetector, revState: this.revState, offset: 0 });
+                fetchAnalysisCount(this.currentDetector);
+              }
+            "
             :class="currentDetector === detector.name ? 'active' : ''"
             class="w-100"
           />
         </div>
       </div>
+
       <div class="row justify-content-center">
         <div class="col-6 d-flex justify-content-between">
           <h5 class="fw-light">Detector: {{ currentDetector }}</h5>
           <!-- ADD: format number method -->
-          <h5 class="fw-light">Analyzed: {{ getAnalysisCount ? formatNumber(getAnalysisCount.count) : ''}} ({{ getDetectorAnalysisPercentage }}%)</h5>
+          <h5 class="fw-light">
+            Analyzed: {{ getAnalysisCount ? formatNumber(getAnalysisCount.count) : "" }} ({{
+              getDetectorAnalysisPercentage
+            }}%)
+          </h5>
         </div>
       </div>
     </div>
     <AnalyticsTableComponent
-            :info="filteredTables"
-            :currentDetector="currentDetector"
-            @re-pass-id="addToPutted"
-            class="analytics-table"
-          />
+      :info="filteredTables"
+      :currentDetector="currentDetector"
+      @re-pass-id="addToPutted"
+      class="analytics-table"
+    />
   </main>
 
   <button
@@ -71,17 +86,19 @@ import StatsComponentVue from "../general/StatsComponent.vue";
 import DetectorComponent from "../general/DetectorComponent.vue";
 import AnalyticsTableComponent from "../general/AnalyticsTableComponent.vue";
 import { mapActions, mapGetters } from "vuex";
+import { AtomSpinner } from "epic-spinners";
 
 export default {
   name: "MainDashboard",
 
-  components: { StatsComponentVue, DetectorComponent, AnalyticsTableComponent },
+  components: { StatsComponentVue, DetectorComponent, AnalyticsTableComponent, AtomSpinner },
 
   data() {
     return {
       currentDetector: "for-continue-increment",
-      revStateFields: ['Unreviewed', 'FP', 'TP_Useless', 'TP_Niceish', 'TP_Explotable'],
+      revStateFields: ["Unreviewed", "FP", "TP_Useless", "TP_Niceish", "TP_Explotable"],
       revState: 0,
+      isDetectorBusy: true,
       showAbort: false,
       abortActive: false,
       lastclickedID: "",
@@ -90,7 +107,14 @@ export default {
   },
 
   computed: {
-    ...mapGetters(["getDetectors", "getTables", "getAuthToken", "getAnalysisCount", "getFlaggedContracts", "getCompilationErrors"]),
+    ...mapGetters([
+      "getDetectors",
+      "getTables",
+      "getAuthToken",
+      "getAnalysisCount",
+      "getFlaggedContracts",
+      "getCompilationErrors",
+    ]),
 
     filteredTables() {
       if (this.puttedRows.length <= 0) {
@@ -102,43 +126,43 @@ export default {
       }
     },
 
-    getTotalErrors(){
-      if(this.getCompilationErrors){
-        return this.getCompilationErrors.reduce((acc, curr)=> acc + curr.count, 0);
+    getTotalErrors() {
+      if (this.getCompilationErrors) {
+        return this.getCompilationErrors.reduce((acc, curr) => acc + curr.count, 0);
       }
     },
 
-    getTotalFlagged(){
-      if(this.getFlaggedContracts){
+    getTotalFlagged() {
+      if (this.getFlaggedContracts) {
         const totalFlagged = [];
-        for(let flagged in this.getFlaggedContracts){
+        for (let flagged in this.getFlaggedContracts) {
           totalFlagged.push(this.getFlaggedContracts[flagged]);
         }
-        return totalFlagged.reduce((acc, curr)=> acc + curr);
+        return totalFlagged.reduce((acc, curr) => acc + curr);
       }
     },
 
-    getDetectorAnalysisPercentage(){
-      if(this.getFlaggedContracts && this.getCompilationErrors && this.getAnalysisCount){
+    getDetectorAnalysisPercentage() {
+      if (this.getFlaggedContracts && this.getCompilationErrors && this.getAnalysisCount) {
         const totalErrors = this.getTotalErrors + this.getTotalFlagged;
         return ((this.getAnalysisCount.count * 100) / totalErrors).toFixed(2);
       }
     },
 
-    orderDetectors(){
+    orderDetectors() {
       if (this.getDetectors) {
-    return this.getDetectors.sort((a, b) => {
-      if (a.name < b.name) {
-        return -1;
+        return this.getDetectors.sort((a, b) => {
+          if (a.name < b.name) {
+            return -1;
+          }
+          if (a.name > b.name) {
+            return 1;
+          }
+          return 0;
+        });
       }
-      if (a.name > b.name) {
-        return 1;
-      }
-      return 0;
-    });
-    }
+    },
   },
- },
 
   methods: {
     //you need to move the put call in here which is triggered by the handle id emit
@@ -162,7 +186,7 @@ export default {
       this.lastclickedID = id;
       this.showAbort = true;
       let abortTimeout = setTimeout(() => {
-        if(this.abortActive && id === this.lastclickedID) {
+        if (this.abortActive && id === this.lastclickedID) {
           this.puttedRows.pop();
           clearTimeout(abortTimeout);
           return;
@@ -172,13 +196,15 @@ export default {
       }, 5000);
     },
 
-    formatNumber(number){
-      if(number){
-        const formattedNumber = number.toLocaleString('en-US', {
-        useGrouping: true,
-        minimumFractionDigits: 0
-      }).replace(/,/g, "'"); //Remove the replace if you want separators at the bottom
-      return formattedNumber;
+    formatNumber(number) {
+      if (number) {
+        const formattedNumber = number
+          .toLocaleString("en-US", {
+            useGrouping: true,
+            minimumFractionDigits: 0,
+          })
+          .replace(/,/g, "'"); //Remove the replace if you want separators at the bottom
+        return formattedNumber;
       }
     },
 
@@ -186,8 +212,8 @@ export default {
       const endpoint = `http://65.108.85.188:3000/api/manualRevision/${id}/${this.currentDetector}/${revState}`;
 
       const headers = {
-        "Accept": "application/json",
-        "authtoken": this.getAuthToken,
+        Accept: "application/json",
+        authtoken: this.getAuthToken,
       };
 
       axios.put(endpoint, null, { headers }).then((res) => {
@@ -202,21 +228,19 @@ export default {
   },
 
   created() {
-    this.fetchDetectors(this.revState);
+    this.fetchDetectors(this.revState).then(() => (this.isDetectorBusy = false));
     this.fetchTables({ detector: this.currentDetector, revState: this.revState, offset: 0 });
     this.fetchAnalysisCount(this.currentDetector);
     this.fetchCompilationErrors();
   },
-
-}
-;
+};
 </script>
 
 <style lang="scss" scoped>
 @use "../../assets/partials/variables" as *;
 
 .ms-radio {
-  &:checked{
+  &:checked {
     background-color: $tertiary-blue;
     border: $secondary-blue;
   }
@@ -236,7 +260,7 @@ export default {
   transition: opacity 6s ease-in-out, right 1s ease-out;
 }
 
-.analytics-table{
+.analytics-table {
   margin: 0 15px;
 }
 </style>
