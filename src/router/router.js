2919f41365d16e3538c7a6ededb522389098f8b3
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-05-17 23:39:51 +0200

    Add: middleware

--- src/router/router.js
+++ src/router/router.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import Authentication from '../components/Autentication.vue';
 import Dashboard from '../components/Dashborad.vue';
 import Home from '../views/Home.vue';
+import { mapGetters } from "vuex";
+import store from '../store/store';
 
 const routes = [
     {
@@ -22,6 +24,20 @@ const router = createRouter({
     history: createWebHistory(),
     routes,
 });
+
+router.beforeEach((to, from, next)=>{
+    const authToken = store.getters.getAuthToken;
+    if(to.path === '/dashboard' && !authToken){
+        next('/login');
+    }else  {
+        next();
+    }
+    if(to.path === '/login' && authToken){
+        next('/dashboard');
+    }else{
+        next();
+    }
+})
 
 
 export default router;
